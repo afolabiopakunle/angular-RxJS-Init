5e56f0d35f75a70f77524c0549b4f870215bd252
afolabiopakunle <opakunleafolabi@gmail.com>
2021-12-02 19:20:17 +0100

    observable from event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component, } from '@angular/core';
+import { AfterViewInit, Component, } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  name = 'Angular ';
+export class AppComponent implements AfterViewInit {
+  
+
+  ngAfterViewInit() {
+   fromEvent(document.querySelector("#sendMessage"), 'click')
+   .pipe()
+   .subscribe({
+     next: data => console.log(data),
+     error: error => console.log(error)
+   }
+   )
+  }
+
 }
